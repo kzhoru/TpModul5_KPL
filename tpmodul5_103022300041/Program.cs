e50377770ae38aeb9b766844838afe2b7c0e3d98
kzhoru <adrianputraperkasa16@gmail.com>
2025-03-17 00:02:14 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022300041/Program.cs
+++ tpmodul5_103022300041/Program.cs
@@ -6,6 +6,21 @@
     }
 }
 
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
 public class Program
 {
     public static void Main()
